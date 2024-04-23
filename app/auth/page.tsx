78ef047226d06b92a4a48da78730a6a6d219c36b
osawa-koki <koki@osawa.dev>
2024-04-24 00:23:41 +0900

    ユーザ属性の表示まで。

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,12 +1,41 @@
 'use client'
 
-import React from 'react'
-import { Button, Table } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Alert, Badge, Button, Table } from 'react-bootstrap'
 
 import { useAuthenticator } from '@aws-amplify/ui-react'
+import { type FetchUserAttributesOutput, fetchUserAttributes } from 'aws-amplify/auth'
 
 export default function App (): React.JSX.Element {
   const { user, signOut } = useAuthenticator((context) => [context.user])
+  const [attributes, setAttributes] = useState<FetchUserAttributesOutput | null>(null)
+
+  useEffect(() => {
+    if (user == null) return
+    fetchUserAttributes()
+      .then((result) => {
+        setAttributes(result)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [user])
+
+  if (user == null) {
+    return (
+      <Alert variant='danger'>
+        You are not signed in.
+      </Alert>
+    )
+  }
+
+  if (attributes == null) {
+    return (
+      <Alert variant='warning'>
+        Fetching user attributes...
+      </Alert>
+    )
+  }
 
   return (
     <>
@@ -19,12 +48,25 @@ export default function App (): React.JSX.Element {
         </thead>
         <tbody>
           <tr>
-            <td>Username</td>
-            <td>{user.username}</td>
+            <td>Sub</td>
+            <td>{attributes.sub}</td>
           </tr>
           <tr>
             <td>Email</td>
-            <td>{user.signInDetails?.loginId}</td>
+            <td>
+              {attributes.email}
+              {attributes.email_verified === 'true'
+                ? (
+                <Badge bg='primary' className='ms-3'>Verified</Badge>
+                  )
+                : (
+                <Badge bg='warning' className='ms-3'>Not Verified</Badge>
+                  )}
+            </td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>{attributes.name}</td>
           </tr>
         </tbody>
       </Table>
