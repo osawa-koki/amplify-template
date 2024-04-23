a67de45bf5eda9d2d4e9a2c9db412c2e3fa73898
osawa-koki <koki@osawa.dev>
2024-04-23 21:34:16 +0900

    公式ドキュメント(https://ui.docs.amplify.aws/react/connected-components/authenticator)通りに進めた。

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,14 @@ import '../styles/menu.scss'
 import setting from '../setting'
 import Menu from '../components/Menu'
 
+import { Amplify } from 'aws-amplify'
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+import awsExports from '../src/aws-exports'
+Amplify.configure(awsExports)
+
 export default function RootLayout ({
   children
 }: {
@@ -38,6 +46,14 @@ export default function RootLayout ({
         )}
       </head>
       <body>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
         <div id="Wrapper">
           <main>{children}</main>
           <Menu currentPage={currentPage} />
