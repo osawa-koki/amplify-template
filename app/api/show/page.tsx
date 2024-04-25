7229a71af2ed783f59a3c7930b14cca8288b607b
osawa-koki <koki@osawa.dev>
2024-04-25 13:48:20 +0900

    todo-showサンプルページの作成。

---
+++ app/api/show/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import { useSearchParams } from 'next/navigation'
+
+import TodoShowComponent from '@/components/pages/api/todo/show'
+
+export default function TodoShow (): React.JSX.Element {
+  const searchParams = useSearchParams()
+
+  const todoId = searchParams.get('todo-id')
+
+  return (
+    <>
+      {JSON.stringify(todoId)}
+      <TodoShowComponent />
+    </>
+  )
+}
