ef74562e8e9b8acb8e88c44f5c7e463b9bc0b094
osawa-koki <koki@osawa.dev>
2024-04-25 14:20:47 +0900

    状態をURLに保持する。

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -1,15 +1,53 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 import { Alert, Form } from 'react-bootstrap'
 
 import TodoComponent from './todo/todo'
 
 type SelectedComponent = '' | 'Todo'
+const allowedSelectedComponents: SelectedComponent[] = ['', 'Todo']
 
 export default function Todos (): React.JSX.Element {
+  // https://nextjs.org/docs/app/api-reference/functions/use-search-params
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
   const [selectedComponent, setSelectedComponent] = useState<SelectedComponent>('')
+  const selected = searchParams.get('selected')
+
+  useEffect(() => {
+    if (allowedSelectedComponents.includes(selected as SelectedComponent)) {
+      setSelectedComponent(selected as SelectedComponent)
+    }
+  }, [])
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
+  useEffect(() => {
+    if (selectedComponent !== '') {
+      router.push(
+        `${pathname}?${createQueryString('selected', selectedComponent)}`,
+        undefined
+      )
+    } else {
+      router.push(
+        pathname,
+        undefined
+      )
+    }
+  }, [selectedComponent])
 
   return (
     <>
